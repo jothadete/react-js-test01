086eb2bf1a2ed76085cdbb7e89ae2420d142b205
Jens Tenter <jens.tenter@live.com>
2022-05-12 09:45:10 +0200

    added files for local use - npm install and ignored them

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ export default function App() {
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some more changing magic happen :)</p>
+      <h2>And run it locally</h2>
+      <p>First install npm into the project folder: npm install</p>
+      <p>And then run (npm start) and/or test (npm test) it. </p>
+      <p>Don´t forget to git-ignore the node_modules...</p>
     </div>
   );
 }
