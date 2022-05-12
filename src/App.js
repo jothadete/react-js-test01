a03b1e8ec653bdf45e23db58544c46314ce3003f
jothadete <jens.tenter@live.com>
2022-05-12 10:54:40 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ export default function App() {
       <p>First install npm into the project folder: npm install</p>
       <p>And then run (npm start) and/or test (npm test) it. </p>
       <p>Don´t forget to git-ignore the node_modules...</p>
+      <h2>Changes on StackBlitz</h2>
+      <p>In StackBlitz added after www.stackblitz.com/ the github-url without the 'https://'. The clicked "Fork". Afterwards "Hard-Connected" to this repo.</p>
+      <p>created new branch. 
     </div>
   );
 }
